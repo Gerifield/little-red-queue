d1a4c2429fe875cc3aaa3f6d1282a6f687d260f0
gerifield <radics.gergely@ustream.tv>
2014-12-14 14:20:48 +0100

    Initial create method tests

--- src/LittleRedQueue/LittleRedQueue.php
+++ src/LittleRedQueue/LittleRedQueue.php
@@ -2,9 +2,38 @@
 
 namespace LittleRedQueue;
 
+use Predis\Client;
+
 /**
  * @see LittleRedQueue\Test\LittleRedQueueTest
  */
 class LittleRedQueue {
 
+	/**
+	 * @var Client
+	 */
+	private $predis;
+
+	public static function create()
+	{
+		return new self(
+			new Client()
+		);
+	}
+
+	public static function createWithConfig($host = '127.0.0.1', $port = 6379, $scheme = 'tcp')
+	{
+		return new self(
+			new Client(array(
+				'scheme' => $scheme,
+				'host'   => $host,
+				'port'   => $port,
+			))
+		);
+	}
+
+	public function __construct(Client $predis)
+	{
+		$this->predis = $predis;
+	}
 }
--- test/LittleRedQueue/LittleRedQueueTest.php
+++ test/LittleRedQueue/LittleRedQueueTest.php
@@ -2,6 +2,44 @@
 
 namespace LittleRedQueue\Test;
 
-class LittleRedQueueTest {
+use LittleRedQueue\LittleRedQueue;
 
+class LittleRedQueueTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var LittleRedQueue
+	 */
+	private $object;
+
+	/**
+	 * @var \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $predis;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->predis = $this->getMockBuilder('\Predis\Client');
+
+		$this->object = new LittleRedQueue(
+			$this->predis->getMock()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createTest()
+	{
+		$this->assertInstanceOf('LittleRedQueue\LittleRedQueue', LittleRedQueue::create());
+	}
+
+	/**
+	 * @test
+	 */
+	public function createWithConfigTest()
+	{
+		$this->assertInstanceOf('LittleRedQueue\LittleRedQueue', LittleRedQueue::createWithConfig());
+	}
 }
